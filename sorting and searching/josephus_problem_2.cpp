164d2db72d05114d63d519a41f3602e64cd08bd1
shreyash369 <107672320+shreyash369@users.noreply.github.com>
2023-09-29 22:48:30 +0530

    Add files via upload

---
+++ sorting and searching/josephus_problem_2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+int main(){
+int n,k; cin>>n>>k;
+tree < int ,  null_type ,  less<int> ,  rb_tree_tag ,  tree_order_statistics_node_update > T;
+for(int i=1;i<=n;i++) T.insert(i);
+// k--;
+int idx=0;
+while(T.size()){
+ idx=(idx+k)%T.size();
+ int x=*T.find_by_order(idx);
+ cout<<x<<endl;
+ T.erase(x);
+}
+return 0;
+}
